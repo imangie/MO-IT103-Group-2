7065462deb1da111d708b27a1769caabdc3987c7
Cherry Angiela <cherryangiela@gmail.com>
2025-06-28 22:59:31 +0900

    Update: Changed labels/title

--- src/main/java/org/example/motorphui/MainApplication.java
+++ src/main/java/org/example/motorphui/MainApplication.java
@@ -2,6 +2,7 @@ package org.example.motorphui;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -19,7 +20,8 @@ public class  MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("hr_dashboard.fxml"));
-        Scene scene = new Scene(fxmlLoader.load()); //
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root, 1440, 970); // Change to any preferred size
 
         stage.setTitle("MotorPH");
 
